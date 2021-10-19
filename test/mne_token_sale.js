14f9035d32c6370d245b3302a58394cd07c182c6
Kevin Mathew <kevinam99@gmail.com>
2021-10-19 23:36:36 +0530

    Initial commit

---
+++ test/mne_token_sale.js
@@ -0,0 +1,10 @@
+const MneTokenSale = artifacts.require("./MneTokenSale.sol");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("MneTokenSale", (accounts) => {
+  it("")
+})
