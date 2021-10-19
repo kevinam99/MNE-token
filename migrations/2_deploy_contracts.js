764aababad38a515d0acb4b321a108f8238795ad
Kevin Mathew <kevinam99@gmail.com>
2021-10-19 23:31:27 +0530

    Add migration for MNE token sale.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MneToken = artifacts.require("./MneToken.sol");
+const MneTokenSale = artifacts.require("./MneTokenSale.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(MneToken, 1000000);
+  deployer.deploy(MneTokenSale);
 };
